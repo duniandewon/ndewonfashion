0990d29c4cc49a79902c1f42e19effb640f54cf0
Mohammad Abolfaz <duniandewon@gmail.com>
2020-08-17 01:27:53 +0700

    If user null show login

--- clinet/src/components/navigations/TopNav.js
+++ clinet/src/components/navigations/TopNav.js
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 
+import { authContext } from '../../context/auth/AuthState';
+
 const TopNav = () => {
+  const { user } = useContext(authContext);
+
   return (
     <div className='top-nav'>
-      <Link className='top-nav__item' to='/login'>
-        <i className='fas fa-user'></i>
-        <span>Login</span>
-      </Link>
+      {user ? (
+        <Link className='top-nav__item' to='/profile'>
+          <i className='fas fa-user'></i>
+          <span>account</span>
+        </Link>
+      ) : (
+        <Link className='top-nav__item' to='/login'>
+          <i className='fas fa-user'></i>
+          <span>Login</span>
+        </Link>
+      )}
       <Link className='top-nav__item' to='/cart'>
         <div className='shopping-cart'>
           <i className='fas fa-shopping-bag'></i>
