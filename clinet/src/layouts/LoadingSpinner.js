9d9b3d601541a83f7ff87224a03ea9807ba00e2f
Mohammad Abolfaz <duniandewon@gmail.com>
2020-08-17 15:46:01 +0700

    Fix: Remove active class and renderspinner if there's no user and loading

--- clinet/src/layouts/LoadingSpinner.js
+++ clinet/src/layouts/LoadingSpinner.js
@@ -4,9 +4,12 @@ import { authContext } from '../context/auth/AuthState';
 const LoadingSpinner = () => {
   const { user, loading } = useContext(authContext);
   return (
-    <h1 className={`loading-spinner ${!user && loading ? 'active' : ''}`}>
-      Bapak bangun melihat ibu mencuci terong.
-    </h1>
+    !user &&
+    loading && (
+      <h1 className='loading-spinner'>
+        Bapak bangun melihat ibu mencuci terong.
+      </h1>
+    )
   );
 };
 
