efbd54a1e972143b2eb9b384ff319435e9bd55e7
Mohammad Abolfaz <duniandewon@gmail.com>
2020-08-26 01:56:10 +0700

    Load user and products

--- clinet/src/pages/ProductCategory.js
+++ clinet/src/pages/ProductCategory.js
@@ -1,17 +1,26 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 
+import { authContext } from '../context/auth/AuthState';
 import { productContext } from '../context/products/ProductsState';
 
 import ProductList from '../components/products/ProductList';
 
 const ProdCategory = () => {
-  const { products } = useContext(productContext);
+  const { loadUser } = useContext(authContext);
+  const { products, getProducts } = useContext(productContext);
+
   const { prod_category } = useParams();
 
-  const prodList = products.filter(
-    (product) => product.category === prod_category
-  );
+  const prodList =
+    products &&
+    products.filter((product) => product.category === prod_category);
+
+  useEffect(() => {
+    loadUser();
+    getProducts();
+    // eslint-disable-next-line
+  }, []);
 
   return <ProductList products={prodList} />;
 };
