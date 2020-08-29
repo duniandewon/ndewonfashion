377030372129f3100679fdfa584233920f9673e6
Mohammad Abolfaz <duniandewon@gmail.com>
2020-08-29 18:32:41 +0700

    Create functions to toggle cart amount and or to update the cart

--- clinet/src/context/cart/cartReducer.js
+++ clinet/src/context/cart/cartReducer.js
@@ -1,5 +1,23 @@
 import { ADD_TO_CART, TOGGLE_AMOUNT, GET_SUBTOTALS } from '../types';
 
+const updateCart = (items, product) =>
+  items.map((item) => {
+    if (item._id === product._id && item.chosenSize === product.chosenSize) {
+      return { ...item, count: product.count };
+    }
+    return item;
+  });
+
+const toggleCartAmount = (items, product, toggle) =>
+  items.map((item) => {
+    if (item._id === product._id) {
+      if (toggle === 'inc') return { ...item, count: item.count + 1 };
+
+      if (toggle === 'dec') return { ...item, count: item.count - 1 };
+    }
+    return item;
+  });
+
 export default (state, action) => {
   switch (action.type) {
     case ADD_TO_CART:
@@ -10,18 +28,16 @@ export default (state, action) => {
 
     case TOGGLE_AMOUNT:
       const { product, toggle } = action.payload;
+
       if (!toggle) {
         return {
           ...state,
-          items: state.items.map((item) => {
-            if (
-              item._id === product._id &&
-              item.chosenSize === product.chosenSize
-            ) {
-              return { ...item, count: product.count };
-            }
-            return item;
-          }),
+          items: updateCart(state.items, product),
+        };
+      } else {
+        return {
+          ...state,
+          items: toggleCartAmount(state.items, product, toggle),
         };
       }
 
